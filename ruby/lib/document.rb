9aec79f27c217220ee15d51202f3cdaaf4bed4dd
julian-salinas <jsalinas@parsimotion.com>
2023-09-23 00:26:17 -0300

    Planteo parte 1

---
+++ ruby/lib/document.rb
@@ -0,0 +1,38 @@
+require_relative 'tag'
+require_relative 'utils/object_mapper'
+
+class Document
+  attr_writer :root_tag
+  def self.serialize(thing)
+    document = new()
+    document.root_tag = document.generate_tag(thing.class.name, thing)
+    document
+  end
+
+  def xml()
+    @root_tag.xml
+  end
+
+  def generate_tag(parent_name, parent_value)
+    attributes_map = ObjectMapper.map_attributes(parent_value)
+    new_tag = Tag.with_label(parent_name)
+    attributes_map.each do |name, value|
+      add_to_tag(new_tag, name, value)
+    end
+    new_tag
+  end
+
+  def add_to_tag(new_tag, name, value)
+    # todo: parte 3: que onda los children?
+    if is_attribute?(value) then new_tag.with_attribute(name, value)
+    else new_tag.with_child(
+      generate_tag name, value
+    )
+    end
+  end
+
+  def is_attribute?(type)
+    primitive_attributes = [Numeric, TrueClass, FalseClass, String]
+    primitive_attributes.any?{ |primitive| type.class <= primitive }
+  end
+end
--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -4,3 +4,28 @@ class Prueba
     :tadp
   end
 end
+
+require_relative 'tag'
+require_relative 'helpers/alumno'
+
+unTag = Tag
+  .with_label('alumno')
+  .with_attribute('nombre', 'Mati')
+  .with_attribute('legajo', '123456-7')
+  .with_attribute('edad', 27)
+  .with_child(
+    Tag
+      .with_label('telefono')
+      .with_child('12345678')
+  )
+  .with_child(
+    Tag
+      .with_label('estado')
+      .with_child(
+        Tag
+          .with_label('value')
+          .with_child('regular')
+      )
+  )
+  .with_child(Tag.with_label('no_children'))
+  .xml
