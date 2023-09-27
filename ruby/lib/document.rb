2a13df2988b2b6e033f4a7553caa1b305072978f
Matias Rossi <matias.rossi@despegar.com>
2023-09-27 18:03:12 -0300

    refactor: split serialization logic in steps

--- ruby/lib/document.rb
+++ ruby/lib/document.rb
@@ -3,26 +3,33 @@ require_relative 'utils/object_mapper'
 
 class Document
   attr_writer :root_tag
-  def self.serialize(thing)
-    document = new
-    document.root_tag = document.generate_tag(thing.class.name, thing)
-    document
-  end
+
 
   def xml
     @root_tag.xml
   end
 
-  def generate_tag(parent_name, parent_value)
+  # Serialización automática
+
+  def create_tag_with_name(name)
+    Tag.with_label(name)
+  end
+
+  def self.generate_tag(parent_name, parent_value)
     attributes_map = ObjectMapper.map_public_attributes(parent_value)
     new_tag = Tag.with_label(parent_name)
-    attributes_map.each do |name, value|
-      add_to_tag(new_tag, name, value)
-    end
+    add_attributes_to_tag(new_tag, attributes_map)
     new_tag
   end
 
-  def add_to_tag(new_tag, name, value)
+  def self.add_attributes_to_tag(tag, attributes_map)
+    attributes_map.each do |name, value|
+      add_to_tag(tag, name, value)
+    end
+    tag
+  end
+
+  def self.add_to_tag(new_tag, name, value)
     # todo: parte 3: que onda los children?
     if is_attribute?(value)
       new_tag.with_attribute(name, value)
@@ -32,8 +39,31 @@ class Document
     end
   end
 
-  def is_attribute?(type)
+  def self.is_attribute?(type)
     primitive_attributes = [Numeric, TrueClass, FalseClass, String]
     primitive_attributes.any?{ |primitive| type.class <= primitive }
   end
+
+  # Serialización manual
+=begin
+
+  private def method_missing(symbol, *args, &block)
+    document.root_tag = document.generate_tag(symbol, args)
+
+    args.each do |key, value|
+      puts "#{key}: #{value}"
+    end
+
+    #super
+  end
+=end
+
+  def self.serialize(thing)
+    document = new
+    new_tag = document.create_tag_with_name(thing.class.name)
+    attributes_map = ObjectMapper.map_public_attributes(thing)
+    document.root_tag = add_attributes_to_tag(new_tag, attributes_map)
+    document
+  end
+
 end
