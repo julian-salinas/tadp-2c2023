f9df6b06966be684db5da7cf5ba4741c11b078ea
julian-salinas <jsalinas@parsimotion.com>
2023-10-06 21:51:07 -0300

    Mas cosas

--- ruby/lib/mapper/block_to_object_mapper.rb
+++ ruby/lib/mapper/block_to_object_mapper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../utils/block_name_extractor'
+require_relative '../utils/type_utils'
 
 class BlockToObjectMapper
 
@@ -11,22 +12,22 @@ class BlockToObjectMapper
   end
 
   private def method_missing(name, *args, **kwargs, &block)
-    puts "method_missing #{name}"
+    puts "method_missing - block_to_object_mapper: (#{name}, #{args}, #{kwargs}, #{block})"
     kwargs.each do |key, value|
-      #puts "key: #{key}, value: #{value}"
       instance_variable_set("@#{key}".to_sym, value)
       self.define_singleton_method(key) { value }
     end
 
     if block_given?
       if TypeUtils.is_primitive? block.call
-        puts "name: #{name}"
+        # =begin
+           # puts "name: #{name}"
         # CHILD FINAL
-        puts "child final: #{BlockNameExtractor.extract(&block)}"
-        instance_variable_set("@#{name}", block.call)
-        puts "asignando #{block.call} a @#{name}"
-        self.define_singleton_method(name) { block.call }
-        #child = ObjectMapper.map_public_attributes(BlockToObjectMapper.map(&block))
+        # puts "child final: #{BlockNameExtractor.extract(&block)}"
+        # instance_variable_set("@#{name}", block.call)
+        # puts "asignando #{block.call} a @#{name}"
+        # self.define_singleton_method(name) { block.call }
+        # child = ObjectMapper.map_public_attributes(BlockToObjectMapper.map(&block))
       else
         # CHILD OBJETO
         puts "child objetoso: #{BlockNameExtractor.extract(&block)}"
