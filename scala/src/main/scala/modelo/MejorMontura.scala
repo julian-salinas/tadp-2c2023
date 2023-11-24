60637dabc65708bc76f7a3ebdff682f7d8276817
Matias Rossi <matias.rossi@despegar.com>
2023-11-24 15:11:08 -0300

    refactor: corrección de tipos en MejorMontura, solo los vikingos pueden encontrarla

--- scala/src/main/scala/modelo/MejorMontura.scala
+++ scala/src/main/scala/modelo/MejorMontura.scala
@@ -1,25 +1,23 @@
 package modelo
 
-import modelo.competidor.Competidor
+import modelo.competidor.{Competidor, Vikingo}
 import modelo.dragon.Dragon
 import modelo.posta.Posta
 import modelo.torneo.{CriterioGanador, Primero}
 
 case object MejorMontura extends (
   (
-    Competidor,
+    Vikingo,
     List[Dragon],
     Posta
   ) => Option[Competidor]) { //Option ya que podría ocurrir que ninguno pueda participar
 
-  def apply(competidor: Competidor,
+  def apply(vikingo: Vikingo,
             dragones: List[Dragon],
             posta: Posta
            ): Option[Competidor] = {
-    val posiblesOpciones: List[Competidor] = dragones.map(competidor.montar).filter(posta.puedeParticipar)
-    val opcionesConCompetidor: List[Competidor] = posiblesOpciones :+ competidor
-    posta.ordenarSegunResultado(opcionesConCompetidor).headOption
-
+    val posiblesOpciones: List[Competidor] = dragones.map(vikingo.montar).filter(posta.puedeParticipar) :+ vikingo
+    posta.ordenarSegunResultado(posiblesOpciones).headOption
   }
 }
 
